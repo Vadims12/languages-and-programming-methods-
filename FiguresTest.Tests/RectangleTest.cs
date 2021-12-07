5255104c7bc499ea994bb01f59e47abc0889935e
Vadims12 <90962157+Vadims12@users.noreply.github.com>
2021-12-07 21:46:46 +0200

    Add files via upload

---
+++ FiguresTest.Tests/RectangleTest.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Diagnostics;
+using Xunit;
+using Figures_L.Figures_L;
+
+namespace FiguresTest.Tests
+{
+    public class RectangleTest
+    {
+        [Fact]
+        public void Generation_of_1000_Objects_And_Their_Hashcodes_Will_Take_Less_Than_One_Minute()
+        {
+            //Arrange
+
+            var rand = new Random();
+            Stopwatch sw = new Stopwatch();
+
+            //Act
+
+            sw.Start();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Rectangle rectangle= new Rectangle();
+                rectangle.GetHashCode();
+            }
+
+            sw.Stop();
+
+            //Assert
+
+            Assert.InRange(sw.ElapsedMilliseconds, 0, 60000);
+        }
+
+        [Fact]
+        public void Generation_of_10000_Objects_And_Their_Hashcodes_Will_Take_Less_Than_One_Minute()
+        {
+            //Arrange
+
+            var rand = new Random();
+            Stopwatch sw = new Stopwatch();
+
+            //Act
+
+            sw.Start();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                Rectangle rectangle = new Rectangle();
+                rectangle.GetHashCode();
+            }
+
+            sw.Stop();
+
+            //Assert
+
+            Assert.InRange(sw.ElapsedMilliseconds, 0, 60000);
+        }
+
+        [Fact]
+        public void Generation_of_100000_Objects_And_Their_Hashcodes_Will_Take_Less_Than_One_Minute()
+        {
+            //Arrange
+
+            var rand = new Random();
+            Stopwatch sw = new Stopwatch();
+
+            //Act
+
+            sw.Start();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                Rectangle rectangle = new Rectangle();
+                rectangle.GetHashCode();
+            }
+
+            sw.Stop();
+
+            //Assert
+
+            Assert.InRange(sw.ElapsedMilliseconds, 0, 60000);
+        }
+
+        [Fact]
+        public void Generation_of_1000000_Objects_And_Their_Hashcodes_Will_Take_Less_Than_One_Minute()
+        {
+            //Arrange
+
+            var rand = new Random();
+            Stopwatch sw = new Stopwatch();
+
+            //Act
+
+            sw.Start();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                Rectangle rectangle = new Rectangle();
+                rectangle.GetHashCode();
+            }
+
+            sw.Stop();
+
+            //Assert
+
+            Assert.InRange(sw.ElapsedMilliseconds, 0, 60000);
+        }
+    }
+}

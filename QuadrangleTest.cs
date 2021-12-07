952ee5133b65284a0b2966dd787d46ab6499dc17
Vadims12 <90962157+Vadims12@users.noreply.github.com>
2021-12-07 21:30:15 +0200

    Add files via upload

---
+++ QuadrangleTest.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Diagnostics;
+using Xunit;
+
+namespace Figures.Tests
+{
+    public class QuadrangleTest
+    {
+        [Fact]
+        public void Generation_of_1000_Objects_And_Their_Hashcodes_Will_Take_Less_Than_One_Minute()
+        {
+            //Arrange
+
+            var rand = new Random();
+            Stopwatch sw = new Stopwatch();
+
+            //Act
+
+            sw.Start();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Quadrangle quadrangle = new Quadrangle(rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 1000000));
+                quadrangle.GetHashCode();
+            }
+
+            sw.Stop();
+
+            //Assert
+
+            Assert.InRange(sw.ElapsedMilliseconds, 0, 60000);
+        }
+
+        [Fact]
+        public void Generation_of_10000_Objects_And_Their_Hashcodes_Will_Take_Less_Than_One_Minute()
+        {
+            //Arrange
+
+            var rand = new Random();
+            Stopwatch sw = new Stopwatch();
+
+            //Act
+
+            sw.Start();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                Quadrangle quadrangle = new Quadrangle(rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 1000000));
+                quadrangle.GetHashCode();
+            }
+
+            sw.Stop();
+
+            //Assert
+
+            Assert.InRange(sw.ElapsedMilliseconds, 0, 60000);
+        }
+
+        [Fact]
+        public void Generation_of_100000_Objects_And_Their_Hashcodes_Will_Take_Less_Than_One_Minute()
+        {
+            //Arrange
+
+            var rand = new Random();
+            Stopwatch sw = new Stopwatch();
+
+            //Act
+
+            sw.Start();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                Quadrangle quadrangle = new Quadrangle(rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 1000000));
+                quadrangle.GetHashCode();
+            }
+
+            sw.Stop();
+
+            //Assert
+
+            Assert.InRange(sw.ElapsedMilliseconds, 0, 60000);
+        }
+
+        [Fact]
+        public void Generation_of_1000000_Objects_And_Their_Hashcodes_Will_Take_Less_Than_One_Minute()
+        {
+            //Arrange
+
+            var rand = new Random();
+            Stopwatch sw = new Stopwatch();
+
+            //Act
+
+            sw.Start();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                Quadrangle quadrangle = new Quadrangle(rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 1000000));
+                quadrangle.GetHashCode();
+            }
+
+            sw.Stop();
+
+            //Assert
+
+            Assert.InRange(sw.ElapsedMilliseconds, 0, 60000);
+        }
+    }
+}

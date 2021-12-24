9422b59fc4db8a7751b40074de0b5e1821decafa
Vadims12 <90962157+Vadims12@users.noreply.github.com>
2021-12-24 12:53:32 +0200

    Add files via upload

---
+++ Контрольная работа по ЯиМП Усков Вадим 203/Employee_program/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_program
+{
+    public class Employee
+    {
+        private List<string> _firstNames = new List<string> { "Владимир", "Петр", "Олег", "Константин", "Александр" };
+        private List<string> _lastNames = new List<string> { "Гордиенко", "Гончар", "Филиппов", "Узун", "Григорьев" };
+        private List<string> _parentNames = new List<string> { "Сегеевич", "Викторович", "Алексеевич", "Васильевич", "Александрович" };
+        private List<string> _departments = new List<string> { "devops", "backend", "gamedev" };
+        private Random _rnd = new Random();
+
+        public readonly string LastName;
+        public readonly string FirstName;
+        public readonly string ParentName;
+        public readonly string Department;
+
+        public int Id { get; }
+        public int Phone { get; }
+
+        public Employee(int id, int phone)
+        {
+            Id = id;
+            Phone = phone;
+
+            LastName = _lastNames[_rnd.Next(0, _lastNames.Count)];
+            FirstName = _firstNames[_rnd.Next(0, _firstNames.Count)];
+            ParentName = _parentNames[_rnd.Next(0, _parentNames.Count)];
+            Department = _departments[_rnd.Next(0, _departments.Count)];
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"{Id.ToString("D4"),-8}{LastName,-16}{FirstName,-14}{ParentName,-18}{Department,-14}{Phone,-7}");
+        }
+    }
+}
---
+++ Контрольная работа по ЯиМП Усков Вадим 203/Employee_program/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Employee_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Random rnd = new Random();
+            for (int i = 0; i < 15; i++)
+            {
+                employees.Add(new Employee(i, rnd.Next(10000, 100000)));
+            }
+            
+            Console.WriteLine($"{0,-8}{1,-16}{2,-14}{3,-18}{4,-14}{5,-7}");
+            Console.WriteLine($"{"Id",-8}{"Фамилия",-16}{"Имя",-14}{"Отчество",-18}{"Отдел",-14}{"Номер телефона",-7}\n");
+            foreach (Employee employee in employees)
+            {
+                employee.Info();
+            }
+
+            Console.Write("\n\nВведите номер столбца для сортировки: ");
+            string num = Console.ReadLine();
+            List<Employee> emplList = new List<Employee>();
+
+            switch (num)
+            {
+                case "0": // Сортировка по id
+                    emplList = employees.OrderBy(x => x.Id).ToList();
+                    break;
+                case "1": // Сортировка по фамилии
+                    emplList = employees.OrderBy(x => x.LastName).ToList();
+                    break;
+                case "2": // Сортировка по имени
+                    emplList = employees.OrderBy(x => x.FirstName).ToList();
+                    break;
+                case "3": // Сортировка по отчеству
+                    emplList = employees.OrderBy(x => x.ParentName).ToList();
+                    break;
+                case "4": // Сортировка по отделу
+                    emplList = employees.OrderBy(x => x.Department).ToList();
+                    break;
+                case "5": // Сортировка по номеру телефона
+                    emplList = employees.OrderBy(x => x.Phone).ToList();
+                    break;
+                default:
+                    Console.WriteLine("Ошибка ввода данных! Введенного номера столбца не существует.");
+                    break;
+            }
+
+            foreach (Employee employee in emplList)
+            {
+                employee.Info();
+            }
+
+            Console.Write("\n\nВведите фамилию для поиска информации: ");
+            string lastName = Console.ReadLine();
+            List<Employee> employeeInfo = employees.Where(x => x.LastName.Equals(lastName)).ToList();
+
+            if (employeeInfo.Count == 0)
+            {
+                Console.WriteLine("Ошибка ввода данных! Введенной фамилии нет в списке.");
+            }
+            else
+            {
+                foreach (Employee employee in employeeInfo)
+                {
+                    employee.Info();
+                }
+            }
+
+            Console.Write("\n\nГруппировка по отделам: ");
+            var emplGroupByDep = employees.GroupBy(x => x.Department).ToList();
+            Console.WriteLine("\n");
+            foreach (IGrouping<string, Employee> group in emplGroupByDep)
+            {
+                Console.WriteLine($"{group.Key}: {group.ToList().Count}");
+                foreach (Employee employee in group)
+                {
+                    employee.Info();
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
